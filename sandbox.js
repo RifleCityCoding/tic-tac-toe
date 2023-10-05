dbefff9d05fce0bf3dc5b0203a8a1049cab578a4
James Spencer <e.spencer@kyspencers.net>
2023-10-05 20:38:44 +0000

    testing if and ifelse instead of switch

--- sandbox.js
+++ sandbox.js
@@ -10,19 +10,61 @@ const rowDiv = document.createElement('div');
 rowDiv.classList.add('row', 'm-0');
 rowDiv.style.height = '420px';
 
+let playerState = 1;
+let winCondition = {
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]
+}
 
 for (let i = 0; i < 9; i++) {
-  const playButton = document.createElement('button');
-  button.classList.add('btn', 'btn-block', 'h-100', 'w-100');
-  button.style.backgroundColor = 'black';
+    const playButton = document.createElement('button');
+    playButton.classList.add('btn', 'btn-block', 'h-100', 'w-100');
+    playButton.style.backgroundColor = 'black';
 
-  const colDiv = document.createElement('div');
-  colDiv.id = i;
-  colDiv.classList.add('col-3', 'p-0');
-  colDiv.style.margin = '1px';
+    const colDiv = document.createElement('div');
+    colDiv.id = i;
+    colDiv.classList.add('col-3', 'p-0');
+    colDiv.style.margin = '1px';
 
-  colDiv.appendChild(playButton);
-  rowDiv.appendChild(colDiv);
+    colDiv.appendChild(playButton);
+    rowDiv.appendChild(colDiv);
+
+    let player1Text = document.createTextNode('X');
+    let player1Span = document.createElement('span');
+    player1Span.style.fontSize = "20px";
+    player1Span.style.color = 'white';
+    player1Span.appendChild(player1Text);
+
+    let player2Text = document.createTextNode('O');
+    let player2Span = document.createElement('span');
+    player2Span.style.fontSize = "20px";
+    player2Span.style.color = 'white';
+    player2Span.appendChild(player2Text);
+    
+ 
+    playButton.addEventListener("click", () => {
+       // playerState = playerState + 1;
+            if (playerState === 0) {
+                playButton.appendChild(player1Span)
+                playerState = 1;
+            }
+
+            else if (playerState === 1) {
+                playButton.appendChild(player2Span)
+                playerState = 0;
+            }
+        // switch (playerState) {
+        //     case 1:
+        //         playButton.appendChild(player1Span)
+        //         playerState = 1;
+        //         if ()
+        //         break;
+        //     case 2:
+        //         playButton.appendChild(player2Span)
+        //         playerState = 0;
+        //         break;
+        // }
+
+    })
 }
 
 innerDiv.appendChild(rowDiv);
@@ -48,19 +90,4 @@ rootElement.appendChild(mainDiv);
 
 
 
-let player1 = document.createTextNode('X')
-player1.style.fontSize = "20px";
-let player2 = document.createTextNode('O')
-player2.style.fontSize = "20px";
-let playerState = 0;
 
-button.addEventListener("click", () => {
-playerState = playerState + 1;
-
-    switch (playerState) {
-        case 1:
-            button.appendChild(player1)
-            break;
-    }
-
-})
